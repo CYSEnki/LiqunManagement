37da15b56e5b8ce0786b99c3948cacbe6cdd8104
unknown <ZA5771@HICHQ.LOCAL>
2023-06-26 20:20:53 +0800

    design sidemenu

--- LiqunManagement/Controllers/LiqunController.cs
+++ LiqunManagement/Controllers/LiqunController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiqunManagement.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +9,29 @@ namespace LiqunManagement.Controllers
 {
     public class LiqunController : Controller
     {
+        //宣告Members資料表的Service物件
+        private readonly MembersDBService membersdbservice = new MembersDBService();
         // GET: Liqun
         public ActionResult Index()
         {
             //判斷使用者是否已經過登入驗證
             if (!User.Identity.IsAuthenticated)
-            {
                 return RedirectToAction("Login", "Members");
-            }
+
+            //var name = User.Identity.Name;
+            //string RoleData = membersdbservice.GetRole(LoginMember.Account);
+            //ViewBag.UserName = 
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Form()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("Login", "Members");
+
+
+
             return View();
         }
     }
