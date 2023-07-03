501d2e2f6d5579bb84dc4d9b12084078ea6ce16a
unknown <ZA5771@HICHQ.LOCAL>
2023-07-03 22:00:56 +0800

    表單設計

---
+++ LiqunManagement/Controllers/AgentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiqunManagement.Controllers
+{
+    public class AgentController : Controller
+    {
+        // GET: Agent
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Form()
+        {
+            return View();
+        }
+    }
+}
