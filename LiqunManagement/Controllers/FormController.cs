e45858528fcc253a1b02915b1a196f6be26b48fa
Enki <xu870710xu@gmail.com>
2023-07-12 23:31:22 +0800

    Form Design and submit to Controller

--- LiqunManagement/Controllers/FormController.cs
+++ LiqunManagement/Controllers/FormController.cs
@@ -35,10 +35,15 @@ namespace LiqunManagement.Controllers
             DDLServices ddlservices = new DDLServices();
             var formmodel = ddlservices.GetRegionDDL("");
             ViewBag.citylist = JsonConvert.SerializeObject(formmodel.regionddl.ToList());
+            List<int> payment_date = new List<int>();
 
+            for(int i=1; i<32; i++)
+            {
+                payment_date.Add(i);
+            }
+            ViewBag.Payment_date = payment_date;
             return View();
         }
-
         [HttpPost]
         public ActionResult HomeObject(
             string Inline1Radio1, 
@@ -49,10 +54,29 @@ namespace LiqunManagement.Controllers
             string district,
             string road,
             string Inline1Radio3,
-            string Inline1Radio3else
+            string Inline1Radio3else,
+            HttpPostedFileBase taxfile,
+            string rent,
+            string deposit,
+            string management_fee,
+            DateTime startdate,
+            DateTime enddate,
+            int paydate,
+            string Inline1Radio4,
+            string Inline1Radio5,
+            string housenumber,
+            string hallnumber,
+            string bathnumber,
+            string Inline1Radio6,
+            string Inline1Radio7,
+            string Inline1Radio8,
+            string carpositionnumber,
+            string carmonthrent,
+            string scootermonthrent,
+            string parkmanagementfee,
+            string scootermanagementfee
             )
         {
-            var taiwan = Specialtext;
             if (ModelState.IsValid)
             {
                 return RedirectToAction("HomeObject", "Form");
